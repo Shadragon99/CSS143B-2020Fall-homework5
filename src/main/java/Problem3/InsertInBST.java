64034b7950f5a0f10988cca75e0f48de99c8f05d
Haaris Nadeem <haaris2001@gmail.com>
2020-11-16 08:07:56 +0500

    Problem 3.1 is complete

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -5,5 +5,29 @@ import Problem1.TreeNode;
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
         // homework
+        TreeNode<Integer> node = new TreeNode(valToInsert);
+
+        TreeNode t = root;
+
+        TreeNode n = null;
+
+        while(t != null){
+            n = t;
+            if(valToInsert < (int) t.val){
+                t = t.left;
+            } else{
+                t = t.right;
+            }
+        }
+
+        if (n == null) {
+            n = node;
+
+        } else if(valToInsert < (int) n.val){
+            n.left = node;
+
+        } else{
+            n.right = node;
+        }
     }
 }
